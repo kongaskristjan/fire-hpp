556520f9646225094e4108dc786bd31f5b42ccb4
Kristjan Kongas <kongaskristjan@gmail.com>
2024-09-07 18:34:49 +0300

    Update licence

--- examples/raw_args.cpp
+++ examples/raw_args.cpp
@@ -1,6 +1,6 @@
 
 /*
-    Copyright (c) 2022 Kristjan Kongas
+    Copyright (c) 2020-2024 Kristjan Kongas
 
     Permission to use, copy, modify, and/or distribute this software for any
     purpose with or without fee is hereby granted.
